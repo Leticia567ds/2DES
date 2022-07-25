496358c17f59b8ffb6275d407adcd02b3b0ce998
Leticia567ds <leticiaamarallopes8@gmail.com>
2022-07-25 11:28:10 -0300

    s

---
+++ Atividade 1/Script.js
@@ -0,0 +1,15 @@
+var placa = document.getElementById("placa");
+
+var placa = {
+    Placa:ABC1212, ABC1B12
+};
+var busca = document.querySelector("#busca");
+var btBusca = document.querySelector("button");
+
+btBusca.addEventListener("click", validarPlaca);
+busca.addEventListener("keyup", validarPlaca);
+
+function validarPlaca() {
+    let invalida = false;
+    
+}
