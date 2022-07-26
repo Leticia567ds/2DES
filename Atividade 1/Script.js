7123cb14a70610d7831ec3cdd372eded83a09e37
Leticia567ds <leticiaamarallopes8@gmail.com>
2022-07-26 11:22:34 -0300

    s

--- Atividade 1/Script.js
+++ Atividade 1/Script.js
@@ -1,15 +1,26 @@
-var placa = document.getElementById("placa");
+// //input
+// var busca = document.querySelector("#busca")
 
-var placa = {
-    Placa:ABC1212, ABC1B12
-};
-var busca = document.querySelector("#busca");
-var btBusca = document.querySelector("button");
+// //botão buscar
+// var btBuscar = document.querySelector('#btn')
+// var res = document.querySelector("p")
 
-btBusca.addEventListener("click", validarPlaca);
-busca.addEventListener("keyup", validarPlaca);
+// btBuscar.addEventListener("click",validarPlaca)
 
-function validarPlaca() {
-    let invalida = false;
-    
-}
+//   function validarPlaca(placa){
+//    const regexPlacaAntiga = /^[a-zA-Z]{3}[0-9]{4}$/
+//    const regexPlacaCarro = /^[a-zA-Z]{3}[0-9]{1}[a-zA-Z]{1}[0-9]{2}$/
+//    const regexPlacaMoto = /^[a-zA-Z]{3}[0-9]{2}[a-zA-Z]{1}[0-9]{1}$/;
+
+//    //iput(busca) e value é o valor dentro
+//    if(regexPlacaAntiga.test(busca.value)){
+//     return res.innerHTML = "Placa válida"
+
+//    }else if(regexPlacaCarro.test(busca.value)){
+//     return res.innerHTML= "Placa válida"
+//    }else if(regexPlacaMoto.test(busca.value)){
+//     return res.innerHTML= "Placa válida"
+//    }else{
+//     return res.innerHTML = "Placa inválida"
+//    }
+//   }
