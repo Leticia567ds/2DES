032d56d57c8a592ce1633b5bfd9c6dd40813bd82
Leticia567ds <leticiaamarallopes8@gmail.com>
2022-11-08 11:20:54 -0300

    s

--- pwbe/estacionamento2/src/script.sql
+++ pwbe/estacionamento2/src/script.sql
@@ -4,14 +4,14 @@ CREATE DATABASE estacionamento charset=UTF8 collate utf8_general_ci;
 USE estacionamento;
 
 CREATE TABLE clientes (
-    id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id_cliente INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
     nome_cliente VARCHAR(50) NOT NULL,
     endereco VARCHAR(60) NOT NULL,
     telefone VARCHAR(10) NOT NULL UNIQUE
 );
 
 CREATE TABLE veiculos (
-    id  INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id_veiculo  INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
     placa VARCHAR(60) NOT NULL UNIQUE,
     tipo VARCHAR(60) NOT NULL
 );
@@ -35,35 +35,28 @@ CREATE TABLE entrada (
 
 
 
-
-
-create view vw_clientes as
-select c.id, c.nome_cliente, c.telefone, c.endereco, v.placa as veiculo from clientes c
-inner join veiculos v
-on c.id = v.placa;
-
-LOAD DATA INFILE 'C:/Users/Desenvolvimento/Desktop/2DES/pwbe/estacionamento/clientes.csv'
+LOAD DATA INFILE 'C:/Users/Desenvolvimento/Desktop/2DES/pwbe/estacionamento2/src/clientes.csv'
 INTO TABLE clientes
 FIELDS TERMINATED BY ';'
 ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS;
 
-LOAD DATA INFILE 'C:/Users/Desenvolvimento/Desktop/2DES/pwbe/estacionamento/veiculo.csv'
+LOAD DATA INFILE 'C:/Users/Desenvolvimento/Desktop/2DES/pwbe/estacionamento2/src/veiculo.csv'
 INTO TABLE veiculos
 FIELDS TERMINATED BY ';'
 ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS;
 
-LOAD DATA INFILE 'C:/Users/Desenvolvimento/Desktop/2DES/pwbe/estacionamento/vagas.csv'
+LOAD DATA INFILE 'C:/Users/Desenvolvimento/Desktop/2DES/pwbe/estacionamento2/src/vagas.csv'
 INTO TABLE vagas
 FIELDS TERMINATED BY ';'
 ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS;
 
-LOAD DATA INFILE 'C:/Users/Desenvolvimento/Desktop/2DES/pwbe/estacionamento/entrada.csv'
+LOAD DATA INFILE 'C:/Users/Desenvolvimento/Desktop/2DES/pwbe/estacionamento2/src/entrada.csv'
 INTO TABLE entrada
 FIELDS TERMINATED BY ';'
 ENCLOSED BY '"'
@@ -74,5 +67,6 @@ select * from clientes;
 select * from veiculos;
 select * from vagas;
 select * from entrada;
+select * from vw_clientes;
 
 
