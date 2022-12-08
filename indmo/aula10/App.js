00c5b34b46846e61c8845388784d5150ca258df8
Leticia567ds <leticiaamarallopes8@gmail.com>
2022-12-08 11:32:46 -0300

    forum atualizado

--- indmo/aula10/App.js
+++ indmo/aula10/App.js
@@ -1,71 +1,103 @@
+// import * as React from 'react';
+// import { Text, View } from 'react-native';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+// function HomeScreen() {
+//   return (
+//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//       <Text>Home!</Text>
+//     </View>
+//   );
+// }
+
+
+
+// function profile() {
+//   return (
+//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//       <Text>Perfil!</Text>
+//     </View>
+//   );
+// }
+
+
+// const Tab = createBottomTabNavigator();
+
+// function MyTabs() {
+//   return (
+//     <Tab.Navigator>
+//       <Tab.Screen name="Home" component={HomeScreen} 
+//         options={{
+//           tabBarLabel: 'Home',
+//           tabBarIcon: ({ color, size }) => (
+//             <MaterialCommunityIcons name="home" color={color} size={size} />
+//           ),
+//         }}
+//       />
+//       <Tab.Screen name="Notification" component={SettingsScreen} 
+//       options={{
+//         tabBarLabel: 'Updates',
+//         tabBarIcon: ({ color, size }) => (
+//           <MaterialCommunityIcons name="bell" color={color} size={size} />
+//         ),
+//         tabBarBadge: "4",
+//       }}/>
+
+//    <Tab.Screen name="Perfil" component={profile} 
+//       options={{
+//         tabBarLabel: 'Profile',
+//         tabBarIcon: ({ color, size }) => (
+//           <MaterialCommunityIcons name="account" color={color} size={size} />
+//         ),
+//       }}/>
+//     </Tab.Navigator>
+//   );
+// }
+
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//       <MyTabs />
+//     </NavigationContainer>
+//   );
+// }
+
+
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Button, View } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-function HomeScreen() {
+function HomeScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
-    </View>
-  );
-}
-
-function SettingsScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Notificação!</Text>
-    </View>
-  );
-}
-
-function profile() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Perfil!</Text>
-    </View>
-  );
-}
-
-
-const Tab = createBottomTabNavigator();
-
-function MyTabs() {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} 
-        options={{
-          tabBarLabel: 'Home',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="home" color={color} size={size} />
-          ),
-        }}
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        onPress={() => navigation.navigate('Notifications')}
+        title="Go to notifications"
       />
-      <Tab.Screen name="Notification" component={SettingsScreen} 
-      options={{
-        tabBarLabel: 'Updates',
-        tabBarIcon: ({ color, size }) => (
-          <MaterialCommunityIcons name="bell" color={color} size={size} />
-        ),
-        tabBarBadge: "4",
-      }}/>
-
-   <Tab.Screen name="Perfil" component={profile} 
-      options={{
-        tabBarLabel: 'Profile',
-        tabBarIcon: ({ color, size }) => (
-          <MaterialCommunityIcons name="account" color={color} size={size} />
-        ),
-      }}/>
-    </Tab.Navigator>
+    </View>
   );
 }
+
+function NotificationsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button onPress={() => navigation.goBack()} title="Go back home" />
+    </View>
+  );
+}
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <MyTabs />
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
